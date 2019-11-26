a91aed22f98c1361a5bfd45443d697f7202b9df2
Matt Belhorn <belhornmp@ornl.gov>
2019-11-26 17:08:08 -0500

    Adds custom OLCF commands to Spack CLI.
    The custom OLCF command adds initially a utility to list the concretized package
    list for an environment with additional verbosity needed to verify packages are
    concretized as intended.
    
    It also adds a deploy subcommand that mimics the standard spack environment
    deploy, but uses the verbose spec listing described above and will ultimately
    show the verbose concretized spec for an explicit spec just prior to starting
    the build.

--- share/spack/extensions/spack-olcf/olcf/cmd/olcf.py
+++ share/spack/extensions/spack-olcf/olcf/cmd/olcf.py
@@ -17,9 +17,12 @@ show-env: Display the long-form concretized specs in a Spack environment lock
 
 
 import sys
+import os
 import argparse
 import llnl.util.tty as tty
+import llnl.util.filesystem as fs
 import spack
+import spack.spec
 import spack.environment as ev
 import spack.cmd
 
@@ -29,33 +32,11 @@ level = "long"
 
 #: List of subcommands of `spack env`
 subcommands = [
-    ['show-env', 'show']
+    ['show-env', 'show'],
+    'deploy',
 ]
 
 subcommand_functions = {}
-
-def setup_parser(subparser):
-    sp = subparser.add_subparsers(metavar='SUBCOMMAND', dest='olcf_command')
-
-    for name in subcommands:
-        if isinstance(name, (list, tuple)):
-            name, aliases = name[0], name[1:]
-        else:
-            aliases = []
-
-        # add commands to subcommands dict
-        function_name = 'olcf_%s' % name.replace('-', '_')
-        function = globals()[function_name]
-        for alias in [name] + aliases:
-            subcommand_functions[alias] = function
-
-        # make a subparser and run the command's setup function on it
-        setup_parser_cmd_name = 'olcf_%s_setup_parser' % name.replace('-', '_')
-        setup_parser_cmd = globals()[setup_parser_cmd_name]
-
-        subsubparser = sp.add_parser(
-            name, aliases=aliases, help=setup_parser_cmd.__doc__)
-        setup_parser_cmd(subsubparser)
 
 
 def display_specs(concretized_specs):
@@ -83,9 +64,55 @@ def display_specs(concretized_specs):
         print('')
 
 
-def olcf_show_env_setup_parser(subparser):
-    """Show concretized specs in an environment"""
+class OLCFEnv(ev.Environment):
+    '''Derived environment class to wrap specific methods.'''
 
+    def _install(self, spec, **install_args):
+        display_specs([spec])
+        spec.package.do_install(**install_args)
+
+        # Make sure log directory exists
+        log_path = self.log_path
+        fs.mkdirp(log_path)
+
+        with fs.working_dir(self.path):
+            # Link the resulting log file into logs dir
+            build_log_link = os.path.join(
+                log_path, '%s-%s.log' % (spec.name, spec.dag_hash(7)))
+            if os.path.lexists(build_log_link):
+                os.remove(build_log_link)
+            os.symlink(spec.package.build_log_path, build_log_link)
+
+
+def setup_parser(subparser):
+    sp = subparser.add_subparsers(metavar='SUBCOMMAND', dest='olcf_command')
+
+    for name in subcommands:
+        if isinstance(name, (list, tuple)):
+            name, aliases = name[0], name[1:]
+        else:
+            aliases = []
+
+        # add commands to subcommands dict
+        function_name = 'olcf_%s' % name.replace('-', '_')
+        function = globals()[function_name]
+        for alias in [name] + aliases:
+            subcommand_functions[alias] = function
+
+        # make a subparser and run the command's setup function on it
+        setup_parser_cmd_name = 'olcf_%s_setup_parser' % name.replace('-', '_')
+        setup_parser_cmd = globals()[setup_parser_cmd_name]
+
+        subsubparser = sp.add_parser(
+            name, aliases=aliases, help=setup_parser_cmd.__doc__)
+        setup_parser_cmd(subsubparser)
+
+
+def olcf_show_env_setup_parser(subparser):
+    """Show concretized specs in the environment"""
+
+def olcf_deploy_setup_parser(subparser):
+    """Install concretized specs in the environment"""
 
 def olcf(parser, args):
     """Look for a function called olcf_<name> and call it."""
@@ -101,3 +128,37 @@ def olcf_show_env(args):
 
     concretized_specs = list(env.concretized_specs())
     display_specs(concretized_specs)
+
+def olcf_deploy(args):
+    '''Install concretized specs in the environment.'''
+    if args.package or args.specfiles:
+        tty.die("olcf deploy acts on an environment, not individual packages")
+    env, env_arg = None, getattr(args, 'env', None)
+    if env_arg:
+        if ev.exists(env_arg):
+            ev.validate_env_name(env_arg)
+            env = OLCFEnv(ev.root(env_arg))
+        elif ev.is_env_dir(env_arg):
+            env = OLCFEnv(env_arg)
+        else:
+            raise SpackEnvironmentError('no environment in %s' % env_arg)
+    if ev._active_environment:
+        env = _active_environment
+    else:
+        tty.die(
+            '`spack olcf deploy` requires an environment',
+            'activate an environment first:',
+            '    spack env activate ENV',
+            'or use:',
+            '    spack -e ENV %s ...' % cmd_name)
+    if env:
+        if not args.only_concrete:
+            # Concretize new specs
+            concretized_specs = env.concretize()
+            display_specs(concretized_specs)
+            env.write()
+        tty.msg("Installing environment %s" % env.name)
+        env.install_all(args)
+        return
+    else:
+        raise ev.SpackEnvironmentError('No environment found')
