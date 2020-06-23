433c708c9f4374bd65cfe63ebc99fd14d37cf012
Matt Belhorn <belhornmp@ornl.gov>
2020-06-23 11:46:39 -0400

    Improves init script
    Removes broken generic init script intended to initialize the environment for
    arbitrary systems. Only the working but peak-specific init script remains.
    
    Comments have been added to the peak-specific init script where it needs to be
    generalized for arbitrary hosts.
    
    The init script is modified to be aware of it's location on the fileystem so it
    can be sourced from any CWD and now uses absolute path references relative to
    it's location rather than relative paths to the CWD.
    
    The spack instance has been moved from the host-specific directories to the root
    directory. It should be possible with environments for all systems to share a
    single spack instance. Lock files for operations on a specific environment
    should exist within host/environment specific paths and prevent conflicts
    arising from multiple systems using the same spack instance simultaneously.

--- init-peak.sh
+++ init-peak.sh
@@ -1,20 +1,36 @@
 #!/bin/bash
 
+#####################
+# Obtain the path to this file.
+#   `_THIS`  : path to this file
+#   `_THIS_DIR`: path to parent directory of ${_THIS}
+# First determine if script is sourced or executed
+if [[ "$0" != "${BASH_SOURCE:-}" ]]; then
+  # Script is sourced
+  _SOURCED=1
+  _THIS="$(which $0)"
+else
+  # Script is executed
+  _SOURCED=0
+  _THIS="${BASH_SOURCE[0]}"
+fi
+while [ -h "$_THIS" ]; do
+  # resolve $_THIS until the file is no longer a symlink
+  _THIS_DIR="$( cd -P "$( dirname "$_THIS" )" >/dev/null && pwd )"
+
+  # if $_THIS was a relative symlink, we need to resolve it relative to the
+  # path where the symlink file was located
+  _THIS="$(readlink "$_THIS")"
+  [[ $_THIS != /* ]] && _THIS="$_THIS_DIR/$_THIS" 
+done
+_THIS_DIR="$( cd -P "$( dirname "$_THIS" )" >/dev/null && pwd )"
+#####################
+
 # This script must be sourced.
-([[ -n $ZSH_EVAL_CONTEXT && $ZSH_EVAL_CONTEXT =~ :file$ ]] || 
- [[ -n $KSH_VERSION && $(cd "$(dirname -- "$0")" &&
-       printf '%s' "${PWD%/}/")$(basename -- "$0") != "${.sh.file}" ]] || 
-  [[ -n $BASH_VERSION ]] && (return 0 2>/dev/null)) && sourced=1 || sourced=0
-
-
-[ ${sourced} -eq 0 ] \
+[ ${_SOURCED} -eq 0 ] \
   && echo "ERROR: This script must be sourced!" \
   && echo "usage: '. ./init-peak.sh'" \
   && exit 1
-
-[ ! -f "./hosts/peak/spack/share/spack/setup-env.sh" ] \
-  && echo "ERROR: This script must be sourced from the root stack manager directory!" \
-  && return 1
 
 # Do nothing if a spack instance is already initialized.
 [[ -n "${SPACK_ROOT:-}" ]] \
@@ -23,42 +39,35 @@
   && echo "Please restart shell to change configuration." \
   && return 1
 
-_spack_host="$(hostname --long \
-               | sed -e 's/\.\(olcf\|ccs\)\..*//' \
-                     -e 's/[-]\?\(login\|ext\|batch\)[^\.]*[\.]\?//' \
-                     -e 's/[-0-9]*$//')"
+_THIS_HOST="$(hostname --long \
+             | sed -e 's/\.\(olcf\|ccs\)\..*//' \
+                   -e 's/[-]\?\(login\|ext\|batch\)[^\.]*[\.]\?//' \
+                   -e 's/[-0-9]*$//')"
 
-# TODO : Setup spack instance in host path if it doesn't exist
-#  - clone spack
-#  - sync hooks
-#  - sync configs/environments
+[[ "${_THIS_HOST:-XX}" == "XX" ]] \
+  && echo "ERROR: Current host '${_THIS_HOST}' could not be identified!" \
+  && return 1
+[ ! -d "${_THIS_DIR}/hosts/${_THIS_HOST}/envs/base" ] \
+  && echo "ERROR: Current host '${_THIS_HOST}' does not have a base environment!" \
+  && return 1
+_HOST_CONF_DIR="${_THIS_DIR}/hosts/${_THIS_HOST}"
 
+# Copy git-tracked modules to module root.
+# FIXME: Need to sync this configuration with the spack environment in use.
 ENV_MODROOT="/sw/.b2/envs/peak/base/modules/lmod"
-
-# Copy git tracked modules to module root.
 mkdir -p "${ENV_MODROOT}"
-cp -dRu --preserve=mode,timestamps "./hosts/${_spack_host}/share/lmod/modulefiles/static/site" "${ENV_MODROOT}/."
-
-# TODO: correct symlinks currently tracked in git repo. This really ought to be an
-# OLCF spack extension.
-if false; then
-for f in $(find "${ENV_MODROOT}/site/Core" \
-    -type f \
-    \( -ipath "*/Core/gcc/*" \
-       -o -ipath "*/Core/xl/*" \
-       -o -ipath "*/Core/llvm/*" \) \
-    -a ! -iname ".*.lua" \
-    -a ! -iname "*-debug.lua"); do
-  echo $f | sed -e "s|^${ENV_MODROOT}/site/Core/||" -e 's|.lua$||'
-done
-fi
-
+cp -dRu --preserve=mode,timestamps "${_HOST_CONF_DIR}/share/lmod/modulefiles/static/site" "${ENV_MODROOT}/."
 
 # Setup alternate module environment
 module purge
+# FIXME: Need to generalize this across multiple hosts.
 export MODULEPATH="${ENV_MODROOT}/spack/linux-rhel7-ppc64le/Core:$ENV_MODROOT/site/Core:/sw/peak/modulefiles/core"
+
+# FIXME: We should use a different python interpretor or a generic one that is
+# portable across all hosts.
 module load python/3.7.0-anaconda3-5.3.0
+
 export PYTHONDONTWRITEBYTECODE=1
-source ./hosts/${_spack_host}/spack/share/spack/setup-env.sh
-echo "Spack initialized for ${_spack_host:-Unknown host} at ${SPACK_ROOT}"
-spack env activate -d /sw/.testing/belhorn/spack/facility-spack/hosts/peak/envs/base
+source "${_THIS_DIR}/spack/share/spack/setup-env.sh"
+echo "Spack initialized for ${_THIS_HOST:-Unknown host} at ${SPACK_ROOT}"
+spack env activate -d "${_HOST_CONF_DIR}/envs/base"
