168f45cab6d1c03721861759bca7f482b40cc811
Matt Belhorn <belhornmp@ornl.gov>
2020-06-30 13:27:37 -0400

    Fixes install_tree root for expanded envvars.
    Querying spack config for an environment's install_tree root does not
    expand environment variables. However, querying the `spack.store`
    database does. This change fixes an error where orphaned packages could
    not be found because spack was looking in a non-existant install_tree
    when the environment was configured to use environment variables to set
    this path.

--- utils/orphans.py
+++ utils/orphans.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env spack-python
 
 import sys
-import pprint
 import itertools
 import spack.store
 import spack.config
 import spack.environment
 
-printp = pprint.PrettyPrinter(indent=4).pprint
 
 query_spec = ''
 
@@ -23,10 +21,9 @@ def installed_at(spec, prefix):
 env = spack.environment._active_environment
 if not env:
     sys.exit(1)
-env_install_prefix = spack.config.get('config:install_tree')
+env_install_prefix = spack.store.store.root
 
-db_args = {'installed': True,
-           'explicit': True}
+db_args = {'installed': True}
 if query_spec:
     db_args['query_spec'] = query_spec
 
@@ -34,13 +31,20 @@ installed_specs = spack.store.db.query_local(**db_args)
 installed_specs_by_hash = {
         spec.build_hash(): spec for spec in installed_specs
         if installed_at(spec, env_install_prefix)}
+
+
+env_spec_list = list()
+for spec_hash in env.concretized_order:
+    spec = env.specs_by_hash[spec_hash]
+    specs = (spec.traverse(deptype=('link', 'build', 'run')))
+    env_spec_list.extend(specs)
+
 env_specs_by_hash = {
-        hash: spec for hash, spec in env.all_specs_by_hash().items()
+        spec.dag_hash(): spec.name for spec in env_spec_list
         if query_filter(spec, query_spec)}
 orphaned_specs = {
         hash: spec for hash, spec in installed_specs_by_hash.items()
         if hash not in env_specs_by_hash.keys()}
-printp([(i.name, i.short_spec, i.prefix) for i in orphaned_specs.values()])
 
 from spack.cmd.uninstall import do_uninstall, confirm_removal, _remove_from_env
 
