cfaebace3886840edc7fb2c93e4c820e8b3d6fc1
Matt Belhorn <belhornmp@ornl.gov>
2020-11-19 09:02:18 -0500

    Adds simple script to generate a changelog.
    A script to generate a changelog between pre-spack-env deployments and
    a facility spack environment has been added. This script is a simple
    quick fix to solve the immediate issue of creating a human-readable list
    of package changes for the Summit RHEL8 upgrade. However, it could be
    improved upon for future upgrades and other systems.
    
    To generate a change log, it is first necessary to capture the list of
    installed packages in both the old Summit RHEL7 deployment and the new
    spack environments on a Summit RHEL8 node.
    
    From a Summit (RHEL7) node:
    ```
    {/sw/summit/.swci/0-core/bin/spack find -lp & /sw/summit/.swci/bin/spack find -lp} \
        | grep "sw/summit" \
        | awk '{print $2}' \
        | sort \
        | uniq > /sw/sources/facspack/sw-manifest.summit-rhel7.20201116.no-comp.log
    ```
    
    From a Summit (RHEL8) node (with spack and environment initialized):
    ```
    spack find -lp \
        | grep "sw/summit" \
        | awk '{print $2}' \
        | sort \
        | uniq > /sw/sources/facspack/sw-manifest.summit.20201116.no-comp.log
    ```
    
    The script added by this commit reads these logs and generates a human
    readable list of changed packages in alphabetical order. It does not
    handle the case where a spack package has been renamed such as
    
    - netcdf --> netcdf-c
    - netcdf-cxx4 --> netcdf-cxx
    - udunits2 --> udunits
    - zeromq --> libzmq
    - etc.
    
    See "https://gitlab.ccs.ornl.gov/ua/spack/-/issues/858" and
    "https://confluence.ccs.ornl.gov/display/UAPre/2020-11-DD+Summit+SP+4.1+Software+Upgrade"
    for more information.

---
+++ utils/changelog.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+
+import pprint
+
+
+def parse_file(path):
+    specs = list()
+    with open(path, 'r') as f:
+        specs = f.readlines()
+    return specs
+
+NEW_SPECS = '/sw/sources/facspack/sw-manifest.summit.20201116.no-comp.log'
+OLD_SPECS = '/sw/sources/facspack/sw-manifest.summit-rhel7.20201116.no-comp.log'
+
+def main():
+    new_specs = parse_file(NEW_SPECS)
+    old_specs = parse_file(OLD_SPECS)
+
+    packages = dict()
+    changelog = dict()
+
+    for spec in new_specs:
+        name, version = spec.split('@')
+        version = version.strip()
+        changes = packages.setdefault(name, dict())
+        new_versions = changes.setdefault('new', set())
+        new_versions.add(version)
+
+    for spec in old_specs:
+        name, version = spec.split('@')
+        version = version.strip()
+        changes = packages.setdefault(name, dict())
+        old_versions = changes.setdefault('old', set())
+        old_versions.add(version)
+    
+    for name, pkg in packages.items():
+        new = pkg.get('new')
+        old = pkg.get('old')
+
+        changes = changelog.setdefault(name, dict())
+        if new and not old:
+            changes['added'] = set(new)
+        elif old and not new:
+            changes['removed'] = set(old)
+        else:
+            changes['added'] = new.difference(old)
+            changes['persisted'] = new.intersection(old)
+            changes['removed'] = old.difference(new)
+
+    for name in sorted(changelog.keys()):
+        pkg = changelog[name]
+        added = pkg.get('added', set())
+        removed = pkg.get('removed')
+        persisted = pkg.get('persisted', set())
+        installed = added.union(persisted)
+        if not installed:
+            print("- {0}".format(name))
+            if installed:
+                print("    installed: {0}".format(', '.join(installed)))
+            if added:
+                print("    added:     {0}".format(', '.join(added)))
+            if removed:
+                print("    removed:   {0}".format(', '.join(removed)))
+    # pp = pprint.PrettyPrinter(indent=4).pprint
+    # pp(changelog)
+
+
+if __name__ == '__main__':
+    main()
