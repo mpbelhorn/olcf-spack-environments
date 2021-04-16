8383b8467d2391de46c9ee5facfe108d4c50a116
Matt Belhorn <belhornmp@ornl.gov>
2021-04-16 10:03:31 -0400

    Adds AOCC v2.3.0 and v3.0.0 to Andes
    This commit adds compiler specs and modulefiles for manually-installed
    new versions of AMD's AOCC toolchain.
    
    New AOCC toolchains v2.3.0 and v3.0.0 have been manually installed from
    source archives located at `/sw/sources/aocc/aocc-compiler-*.tar`.
    Sources are fetched from the [AOCC distribution homepage on AMD's
    developer portal](https://developer.amd.com/amd-aocc/#download). The
    archives are manually installed per the script:
    
    ```
    /sw/andes/aocc/.install/install.sh 2.3.0
    /sw/andes/aocc/.install/install.sh 3.0.0
    ```
    
    Contents of the auto-generated environment setup script provided by AOCC
    `/sw/andes/aocc/*/setenv_AOCC.sh` is used as the template to write the
    modulefiles added by this commit.

---
+++ hosts/andes/share/lmod/modulefiles/static/site/Core/aocc/3.0.0.lua
@@ -0,0 +1,37 @@
+family("compiler")
+
+help([[
+AMD AOCC Compiler
+]])
+
+
+local package = "aocc"
+local version = "3.0.0"
+local prefix = pathJoin("/sw/andes/aocc", version, "/aocc-compiler-" .. version)
+local platform = 'linux-rhel8-x86_64'
+local spack_module_root, _ = myFileName():gsub('/site/Core/'..myModuleFullName()..'.*', '/spack')
+local spack_modules = pathJoin(spack_module_root, platform, myModuleFullName())
+
+whatis("Description: ", "AMD AOCC EPYC Optimizing compiler " .. version)
+
+-- Setup Modulepath for packages built by this compiler
+prepend_path( "MODULEPATH", spack_modules)
+
+-- Environment Globals
+prepend_path( "PATH",  pathJoin(prefix, "bin" ) )
+
+append_path("LD_LIBRARY_PATH", pathJoin(prefix, "lib"))
+append_path("LD_LIBRARY_PATH", pathJoin(prefix, "lib32"))
+-- prepend_path("LD_LIBRARY_PATH", "/usr/lib64:/usr/lib")
+
+append_path("LIBRARY_PATH", pathJoin(prefix, "lib"))
+append_path("LIBRARY_PATH", pathJoin(prefix, "lib32"))
+-- prepend_path("LIBRARY_PATH", "/usr/lib64:/usr/lib")
+
+append_path("C_INCLUDE_PATH", pathJoin(prefix, "include"))
+append_path("CPLUS_INCLUDE_PATH", pathJoin(prefix, "include"))
+
+-- prepend_path( "MANPATH", pathJoin(gccdir, "share/man" ) )
+
+-- OLCF specific Environment
+setenv("OLCF_AOCC_ROOT",  prefix)
