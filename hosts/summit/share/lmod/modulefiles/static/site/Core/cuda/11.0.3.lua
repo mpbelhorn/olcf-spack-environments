ac6bf3c2024735f4e716d5692244b30bb504074d
Matt Belhorn <belhornmp@ornl.gov>
2021-09-03 15:11:10 -0400

    Ports CUDA modules from TCL to lua

---
+++ hosts/summit/share/lmod/modulefiles/static/site/Core/cuda/11.0.3.lua
@@ -0,0 +1,23 @@
+-- -*- lua -*-
+
+local package = "cuda"
+local version = "11.0.3"
+local host = "summit"
+local prefix = pathJoin("/sw", host, package, version)
+
+whatis([[Changes the CUDA module]])
+help([[ Tools to use CUDA
+
+        Version ]] .. version .. [[
+
+]])
+
+try_load("nsight-compute", "nsight-systems")
+prepend_path("PATH", pathJoin(prefix, "bin"))
+prepend_path("LD_LIBRARY_PATH", pathJoin(prefix, "lib64"))
+prepend_path("MANPATH", pathJoin(prefix, "doc/man"))
+prepend_path("CPATH", pathJoin(prefix, "include"))
+setenv("CUDA_DIR", prefix)
+setenv("CUDA_TOOLKIT_ROOT_DIR", prefix)
+setenv("OLCF_CUDA_ROOT", prefix)
+setenv("CUDAPATH", prefix)
