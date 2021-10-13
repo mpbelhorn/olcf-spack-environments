3db3fc46056b5a6b324b29596a8e77dcddeb165c
Matt Belhorn <belhornmp@ornl.gov>
2021-10-13 10:50:03 -0400

    Updates Spack to v0.16.3 in preparation for adding E4S 21.08

---
+++ hosts/spock/envs/base/view-generate-rocm-4.1.0.sh
@@ -0,0 +1,164 @@
+#!/bin/bash
+
+ROCM_VERSION="4.1.0"
+VIEW_ROOT="/sw/spock/spack-envs/views/rocm-${ROCM_VERSION}"
+# Alternate view for debugging
+# VIEW_ROOT="/tmp/belhorn/view_tests/rocm-${ROCM_VERSION}"
+
+######################### 
+VIEW_PREFIX="$(dirname ${VIEW_ROOT})"
+VIEW_NAME="$(basename ${VIEW_ROOT})"
+
+# FIXME - replace with a tarball or script.
+PATCH_LINKS="/sw/spock/spack-envs/views/.rocm_symlinks"
+
+
+if [[ "$0" == "${BASH_SOURCE:-}" ]]; then
+  echo "This script must be sourced"
+  exit 1
+fi
+
+_PREVIOUS_VIEW="${VIEW_PREFIX:-/tmp/facspack-view-regen}/.${VIEW_NAME}.last"
+if [ -e "${_PREVIOUS_VIEW}.tmp" ]; then
+  echo "Previous failed update exists. Cancelling."
+  return 1
+fi
+
+# Exit if facility spack environment management runtime is not setup
+if [ -z "${FACSPACK_SPACK_ROOT:-}" ]; then
+  echo "Facspack not initialized"
+  return 1
+elif [ -z "${SPACK_ENV:-}" ]; then
+  echo "Not in an active spack environment"
+  return 1
+elif [[ "${FACSPACK_HOST}" != "spock" ]]; then
+  echo "Not running on Spock"
+  return 1
+fi
+
+# Specific specs, when needed are selected from the output of:
+# ```
+# ROCM_VERSION=4.1.0 . ./find-rocm-specs.sh 
+# ```
+
+# View conflicts are resolved in first-come:first-used manner. Conflicting specs
+# must be added to the view in multiple rounds
+SPECS=""
+SPECS+=" 'comgr@${ROCM_VERSION}%gcc@7.5.0'"
+SPECS+=" 'hip@${ROCM_VERSION}%gcc@7.5.0'"
+SPECS+=" 'hip-rocclr@${ROCM_VERSION}%gcc@7.5.0'"
+SPECS+=" 'hipblas@${ROCM_VERSION}%gcc@7.5.0'"
+SPECS+=" 'hipcub@${ROCM_VERSION}%gcc@7.5.0'"
+SPECS+=" 'hipfft@${ROCM_VERSION}%gcc@7.5.0'"
+SPECS+=" 'hipfort@${ROCM_VERSION}%gcc@7.5.0'"
+# SPECS+=" 'hipify-clang@${ROCM_VERSION}%gcc@7.5.0'"
+SPECS+=" 'hipsparse@${ROCM_VERSION}%gcc@7.5.0'"
+SPECS+=" 'hsa-rocr-dev@${ROCM_VERSION}%gcc@7.5.0'"
+SPECS+=" 'hsakmt-roct@${ROCM_VERSION}%gcc@7.5.0'"
+# SPECS+=" 'llvm-amdgpu@${ROCM_VERSION}%gcc@7.5.0'"
+SPECS+=" 'miopen-hip@${ROCM_VERSION}%gcc@7.5.0'"
+SPECS+=" 'rccl@${ROCM_VERSION}%gcc@7.5.0'"
+SPECS+=" 'rocalution@${ROCM_VERSION}%gcc@7.5.0'"
+SPECS+=" 'rocblas@${ROCM_VERSION}%gcc@7.5.0'"
+SPECS+=" 'rocfft@${ROCM_VERSION}%gcc@7.5.0'"
+SPECS+=" 'rocm-clang-ocl@${ROCM_VERSION}%gcc@7.5.0'"
+SPECS+=" 'rocm-cmake@${ROCM_VERSION}%gcc@7.5.0'"
+SPECS+=" 'rocm-device-libs@${ROCM_VERSION}%gcc@7.5.0'"
+SPECS+=" 'rocm-gdb@${ROCM_VERSION}%gcc@7.5.0'"
+SPECS+=" 'rocm-dbgapi@${ROCM_VERSION}%gcc@7.5.0'"
+# SPECS+=" 'rocm-opencl@${ROCM_VERSION}%gcc@7.5.0'"
+SPECS+=" 'rocm-opencl-runtime@${ROCM_VERSION}%gcc@7.5.0'"
+SPECS+=" 'rocm-openmp-extras@${ROCM_VERSION}%gcc@7.5.0'"
+# SPECS+=" 'rocm-smi@${ROCM_VERSION}%gcc@7.5.0'" ## Not installed for v4.1.0+
+SPECS+=" 'rocm-smi-lib@${ROCM_VERSION}%gcc@7.5.0'"
+SPECS+=" 'rocminfo@${ROCM_VERSION}%gcc@7.5.0'"
+SPECS+=" 'rocprim@${ROCM_VERSION}%gcc@7.5.0'"
+SPECS+=" 'rocprofiler-dev@${ROCM_VERSION}%gcc@7.5.0'"
+SPECS+=" 'rocrand@${ROCM_VERSION}%gcc@7.5.0'"
+SPECS+=" 'rocsolver@${ROCM_VERSION}%gcc@7.5.0'"
+SPECS+=" 'rocsparse@${ROCM_VERSION}%gcc@7.5.0'"
+SPECS+=" 'rocthrust@${ROCM_VERSION}%gcc@7.5.0'"
+# SPECS+=" 'roctracer-dev@${ROCM_VERSION}%gcc@7.5.0'" ## Not installed for v4.1.0
+
+VIEW_SPECS_ROUND_ONE="${SPECS}"
+VIEW_SPECS_ROUND_TWO="'llvm-amdgpu@${ROCM_VERSION}%gcc@7.5.0'"
+
+[ ! -d "${VIEW_PREFIX}" ] && mkdir -p "${VIEW_PREFIX}"
+TMP_VIEW_ROOT=$(mktemp -d -p ${VIEW_PREFIX} XXXXXX)
+[ -d "${TMP_VIEW_ROOT}" ] && chmod go=rx "${TMP_VIEW_ROOT}"
+spack view \
+    --dependencies 'false' \
+    --verbose \
+    symlink --ignore-conflicts \
+            --projection-file "hosts/spock/envs/base/view-rocm-${ROCM_VERSION}.yaml" \
+            "${TMP_VIEW_ROOT}" \
+            ${VIEW_SPECS_ROUND_ONE}
+
+if [ "$?" -eq 0 ]; then
+  spack view \
+      --dependencies 'false' \
+      --verbose \
+      symlink --ignore-conflicts \
+              --projection-file "hosts/spock/envs/base/view-rocm-${ROCM_VERSION}.yaml" \
+              "${TMP_VIEW_ROOT}" \
+              ${VIEW_SPECS_ROUND_TWO}
+
+  if [ "$?" -eq 0 ]; then
+    # Handle special cases with spack making empty dirs in the view for relative
+    # symlinks under a package's install prefix.
+    [ -d "${TMP_VIEW_ROOT}/hsa/include/hsa" ] && rmdir --ignore-fail-on-non-empty "${TMP_VIEW_ROOT}/hsa/include/hsa"
+    # FIXME - replace with a tarball or script.
+    cp -vRP ${PATCH_LINKS}/bin \
+            ${PATCH_LINKS}/include \
+            ${PATCH_LINKS}/lib \
+            ${PATCH_LINKS}/hsa \
+            ${PATCH_LINKS}/oam \
+            -t ${TMP_VIEW_ROOT}
+
+    if [ "$?" -eq 0 ]; then
+      if [ -e "${_PREVIOUS_VIEW}.tmp" ]; then
+        rm -fr "${TMP_VIEW_ROOT}"
+        echo "ERROR: Previous view backup exists '${_PREVIOUS_VIEW}.tmp'."
+        return 1
+      elif [ -d "${_PREVIOUS_VIEW}" ]; then
+        /usr/bin/mv -Tn "${_PREVIOUS_VIEW}" "${_PREVIOUS_VIEW}.tmp"
+      fi
+      
+      if [ -e "${_PREVIOUS_VIEW}" ]; then
+        echo "ERROR: Previous view still exists '${_PREVIOUS_VIEW}'."
+        return 1
+      elif [ -d "${VIEW_ROOT}" ]; then
+        /usr/bin/mv -Tn "${VIEW_ROOT}" "${_PREVIOUS_VIEW}" 
+      fi
+      
+      if [ -e "${VIEW_ROOT}" ]; then
+        echo "ERROR: Current view still exists '${_PREVIOUS_VIEW}'."
+        return 1
+      elif [ -d "${TMP_VIEW_ROOT}" ]; then
+        /usr/bin/mv -Tn "${TMP_VIEW_ROOT}" "${VIEW_ROOT}"
+      fi
+      
+      if [ -e "${_PREVIOUS_VIEW}.tmp" ]; then
+        # echo "DEBUG: Removing prior view backup '${_PREVIOUS_VIEW}.tmp'"
+        /usr/bin/rm -fr "${_PREVIOUS_VIEW}.tmp"
+      fi
+      
+      echo "Successfully updated view '${VIEW_ROOT}'."
+    else
+      # Failed symlink copy
+      echo "Failed to copy out relative symlink tree."
+      rm -fr "${TMP_VIEW_ROOT}"
+      return 1
+    fi
+  else
+    # Failed second round
+    echo "Failed second round of view generation."
+    rm -fr "${TMP_VIEW_ROOT}"
+    return 1
+  fi
+else
+  # Failed first round
+  echo "Failed first round of view generation."
+  rm -fr "${TMP_VIEW_ROOT}"
+  return 1
+fi
--- share/spack/repos/olcf/packages/vmd/package.py
+++ share/spack/repos/olcf/packages/vmd/package.py
@@ -1,32 +1,61 @@
-# Copyright 2013-2019 Lawrence Livermore National Security, LLC and other
+# Copyright 2013-2021 Lawrence Livermore National Security, LLC and other
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-from spack import *
 import os
 
+from spack import *
+
+
 class Vmd(Package):
-    """VMD is designed for modeling, visualization, and analysis of biological
-    systems such as proteins, nucleic acids, lipid bilayer assemblies, etc. It may
-    be used to view more general molecules, as VMD can read standard Protein Data
-    Bank (PDB) files and display the contained structure. VMD can act
-    as a graphical front end for an external MD program by displaying and animating
-    a molecule undergoing simulation on a remote computer.
+    """VMD provides user-editable materials which can be applied
+    to molecular geometry.
+
+    These material properties control the details of how VMD shades
+    the molecular geometry, and how transparent or opaque the displayed
+    molecular geometry is. With this feature, one can easily create nice
+    looking transparent surfaces which allow inner structural details to
+    be seen within a large molecular structure. The material controls can
+    be particularly helpful when rendering molecular scenes using external
+    ray tracers, each of which typically differ slightly.
     """
+
     homepage = "https://www.ks.uiuc.edu/Research/vmd/"
     url      = "http://www.ks.uiuc.edu/Research/vmd/vmd-1.9.3/files/final/vmd-1.9.3.bin.LINUXAMD64-CUDA8-OptiX4-OSPRay111p1.opengl.tar.gz"
-
     version('1.9.3',
             sha256sum='9427a7acb1c7809525f70f635bceeb7eff8e7574e7e3565d6f71f3d6ce405a71',
             url='http://www.ks.uiuc.edu/Research/vmd/vmd-1.9.3/files/final/vmd-1.9.3.bin.LINUXAMD64-CUDA8-OptiX4-OSPRay111p1.opengl.tar.gz')
 
+    phases = ['configure', 'install']
+
+    depends_on('libx11', type=('run', 'link'))
+    depends_on('libxi', type=('run', 'link'))
+    depends_on('libxinerama', type=('run', 'link'))
+    depends_on('gl@3:', type=('run', 'link'))
+    depends_on('patchelf', type='build')
+
+    def setup_build_environment(self, env):
+        env.set('VMDINSTALLBINDIR', self.prefix.bin)
+        env.set('VMDINSTALLLIBRARYDIR', self.prefix.lib64)
+
+    def configure(self, spec, prefix):
+        configure = Executable('./configure')
+        configure('LINUXAMD64')
+
     def install(self, spec, prefix):
-      env = os.environ
-      env["VMDINSTALLBINDIR"] = prefix.bin
-      env["VMDINSTALLLIBRARYDIR"] = prefix.lib
+        with working_dir("src"):
+            make("install")
 
-      configure()
+        # make sure the executable finds and uses the Spack-provided
+        # libraries, otherwise the executable may or may not run depending
+        # on what is installed on the host
+        patchelf = which('patchelf')
+        rpath = ':'.join(
+            self.spec[dep].libs.directories[0]
+            for dep in ['libx11', 'libxi', 'libxinerama', 'gl'])
+        patchelf('--set-rpath', rpath,
+                 join_path(self.prefix, 'lib64', 'vmd_LINUXAMD64'))
 
-      with working_dir("src"):
-        make("install", env=env)
+    def setup_run_environment(self, env):
+        env.set('PLUGINDIR', self.spec.prefix.lib64.plugins)
