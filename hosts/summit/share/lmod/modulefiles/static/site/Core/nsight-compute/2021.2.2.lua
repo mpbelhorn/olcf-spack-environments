6cc7810b7271e2afb1dcb28713bb34846a874795
Matt Belhorn <belhornmp@ornl.gov>
2022-02-08 10:49:53 -0500

    Updates Nvidia Nsight module path priority
    See [OLCFSW-154](https://jira.ccs.ornl.gov/browse/OLCFSW-154). Nsys and
    other binaries shipped with both CUDA and nsight-systems/nsight-compute
    should always prefer binaries from nsight-systems/nsight-compute
    regardless of module load order.
    
    Replaces existing TCL modules for Nsight-systems and Nsight-compute with
    Lua modules supporting path priority ordering options.

---
+++ hosts/summit/share/lmod/modulefiles/static/site/Core/nsight-compute/2021.2.2.lua
@@ -0,0 +1,15 @@
+local host    = "summit"
+local version = "2021.2.2"
+local pkg     = "nsight-compute"
+local prefix  = pathJoin("/sw", host, pkg, version)
+
+-- Info
+help([[
+Tools to use Nsight Compute
+Version ]] .. version
+)
+whatis("Nvidia Nsight Compute")
+
+-- Paths
+prepend_path{"PATH", pathJoin(prefix, "bin"), priority=9999}
+setenv("OLCF_NSIGHT_COMPUTE_ROOT", prefix) 
---
+++ hosts/summit/share/lmod/modulefiles/static/site/Core/nsight-systems/2021.3.1.54.lua
@@ -0,0 +1,15 @@
+local host    = "summit"
+local version = "2021.3.1.54"
+local pkg     = "nsight-systems"
+local prefix  = pathJoin("/sw", host, pkg, version)
+
+-- Info
+help([[
+Tools to use Nvidia Nsight Systems
+Version ]] .. version
+)
+whatis("Nvidia Nsight Systems")
+
+-- Paths
+prepend_path{"PATH", pathJoin(prefix, "bin"), priority=100}
+setenv("OLCF_NSIGHT_SYSTEMS_ROOT", prefix) 
