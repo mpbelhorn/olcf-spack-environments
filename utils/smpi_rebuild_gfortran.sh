c93be76b240f462b474658ecb4fea0dcb29b9315
Matt Belhorn <belhornmp@ornl.gov>
2022-06-21 14:04:25 -0400

    Updates smpi fortran module rebuild script to handle case where SMPI needing patching is not the one pointed at by an available modulefile.

--- utils/smpi_rebuild_gfortran.sh
+++ utils/smpi_rebuild_gfortran.sh
@@ -2,10 +2,11 @@
 
 _COMPILER_MODULE="${1:-}"
 _SMPI_MODULE="${2:-spectrum-mpi}"
+_PREFIX="${3:-}"
 
 if [[ -z "${_COMPILER_MODULE}" || -z "${_SMPI_MODULE}" ]]; then
   echo "USAGE: ./$0 COMPILER_MODULE [SMPI_MODULE]"
-  echo "   eg: ./$0 \"gcc/7.5.0\" [\"spectrum-mpi/default\"]"
+  echo "   eg: ./$0 \"gcc/7.5.0\" [\"spectrum-mpi/default\"] [PREFIX]"
   exit 1
 fi
 
@@ -14,12 +15,16 @@ set -e
 
 module purge
 module load "${_COMPILER_MODULE}"
-
-# Get SMPI dir from module
-module load "${_SMPI_MODULE}"
-PREFIX="${OLCF_SPECTRUM_MPI_ROOT}"
-# PREFIX="/sw/ascent/spack-envs/base/opt/linux-rhel8-ppc64le/gcc-7.5.0/spectrum-mpi-10.4.0.3-20210112-puowkoejepfjtm22sk2dxb6eeup5w447"
-module unload spectrum-mpi
+if [[ -z "${_PREFIX}" ]]; then
+  # Get SMPI dir from module
+  module load "${_SMPI_MODULE}"
+  PREFIX="${OLCF_SPECTRUM_MPI_ROOT}"
+  # PREFIX="/sw/ascent/spack-envs/base/opt/linux-rhel8-ppc64le/gcc-7.5.0/spectrum-mpi-10.4.0.3-20210112-puowkoejepfjtm22sk2dxb6eeup5w447"
+  module unload spectrum-mpi
+else
+  PREFIX="${_PREFIX}"
+  echo "Using manually specified prefix: '${PREFIX}'"
+fi
 
 # Locate compilers and set SMPI fortran module rebuild parameters
 case "${_COMPILER_MODULE%%/*}" in
@@ -82,13 +87,13 @@ cd "${PREFIX}/lib/fortsupport_rebuild"
 cd "${PREFIX}/lib/fortsupport_rebuild"
 [ ! -f "${PREFIX}/include/mpif-sizeof-stock.h" ] \
   && mv "${PREFIX}/include/mpif-sizeof.h" "${PREFIX}/include/mpif-sizeof-stock.h"
-cp ./mpif-sizeof.h "${PREFIX}/include/mpif-sizeof.h"
+cp -u ./mpif-sizeof.h "${PREFIX}/include/mpif-sizeof.h"
 ################
 
 ################
 # Install new fortran modules in their default locations
 cd "${PREFIX}/lib/fortsupport_rebuild"
-cp *.mod  "${PREFIX}/lib/fortsupport_${_SUFFIX}/."
+cp -u *.mod  "${PREFIX}/lib/fortsupport_${_SUFFIX}/."
 # cp mpi.mod                          "${PREFIX}/lib/fortsupport_{_SUFFIX}/."
 # cp mpi_f08.mod                      "${PREFIX}/lib/fortsupport_{_SUFFIX}/."
 # cp mpi_f08_types.mod                "${PREFIX}/lib/fortsupport_{_SUFFIX}/."
@@ -102,20 +107,20 @@ chmod a+x "${PREFIX}/lib/fortsupport_${_SUFFIX}"/*
 ################
 # Install new fortran libraries in their default locations
 cd "${PREFIX}/lib/fortsupport_rebuild"
-cp "libmpi_ibm_usempi_ignore_tkr_custom.so"        "${PREFIX}/lib/."
-cp "libmpi_ibm_usempi_ignore_tkr_custom_archive.a" "${PREFIX}/lib/."
-cp "libmpi_ibm_usempif08_custom.so"                "${PREFIX}/lib/."
-cp "libmpi_ibm_usempif08_custom_archive.a"         "${PREFIX}/lib/."
+cp -u "libmpi_ibm_usempi_ignore_tkr_custom.so"        "${PREFIX}/lib/."
+cp -u "libmpi_ibm_usempi_ignore_tkr_custom_archive.a" "${PREFIX}/lib/."
+cp -u "libmpi_ibm_usempif08_custom.so"                "${PREFIX}/lib/."
+cp -u "libmpi_ibm_usempif08_custom_archive.a"         "${PREFIX}/lib/."
 
 # Backup existing fortran libraries/archives and update symlinks to new libs
 cd "${PREFIX}/lib"
 [ ! -f  "libmpi_ibm_usempif08_${_SUFFIX}_stock.so" ] \
   && mv "libmpi_ibm_usempif08_${_SUFFIX}.so" \
         "libmpi_ibm_usempif08_${_SUFFIX}_stock.so"
-ln -s   "libmpi_ibm_usempif08_custom.so" "libmpi_ibm_usempif08_${_SUFFIX}.so"
+ln -fs   "libmpi_ibm_usempif08_custom.so" "libmpi_ibm_usempif08_${_SUFFIX}.so"
 
 [ ! -f  "libmpi_ibm_usempi_ignore_tkr_${_SUFFIX}_stock.so" ] \
   && mv "libmpi_ibm_usempi_ignore_tkr_${_SUFFIX}.so" \
         "libmpi_ibm_usempi_ignore_tkr_${_SUFFIX}_stock.so"
-ln -s   "libmpi_ibm_usempi_ignore_tkr_custom.so" "libmpi_ibm_usempi_ignore_tkr_${_SUFFIX}.so"
+ln -fs  "libmpi_ibm_usempi_ignore_tkr_custom.so" "libmpi_ibm_usempi_ignore_tkr_${_SUFFIX}.so"
 ################
